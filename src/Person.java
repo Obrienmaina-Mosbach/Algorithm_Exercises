6f5a61e49660b2ad058b2e9dd1b9495fdb10c983
Brian Nyawira <191464653+Obrienmaina-Mosbach@users.noreply.github.com>
2025-05-27 18:33:10 +0200

    First getter and setter method

---
+++ src/Person.java
@@ -0,0 +1,56 @@
+public class Person {
+
+    private int personID;
+    private String firstName;
+    private String surName;
+    private int personAge;
+
+    public Person(int personID, String firstName, String surName, int personAge){
+        this.personID = personID;
+        this.firstName = firstName;
+        this.surName = surName;
+        this.personAge = personAge;
+    }
+
+    //getters
+    public int getPersonID() {
+        return personID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public int getPersonAge() {
+        return personAge;
+    }
+
+    //setters
+    public void setPersonID(int newPersonID){
+        this.personID = newPersonID;
+    }
+
+    public void setFirstName(String newFirstName){
+        this.firstName = newFirstName;
+    }
+
+    public void setSurName(String newSurname){
+        this.surName = newSurname;
+    }
+
+    public void setPersonAge(int newPersonAge){
+        this.personAge = newPersonAge;
+    }
+
+    public static void main(String[] args){
+        Person myNewPersonObject = new Person(9843, "Ulia", "Mendez", 19);
+        myNewPersonObject.setPersonAge(14);
+        System.out.println(myNewPersonObject.getPersonAge());
+    }
+
+
+}
