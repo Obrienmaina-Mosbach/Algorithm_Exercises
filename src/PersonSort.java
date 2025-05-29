ce00e06e933b8ebdac148af1d2cbd9ee7379193c
Brian Nyawira <191464653+Obrienmaina-Mosbach@users.noreply.github.com>
2025-05-29 20:40:06 +0200

    Bubble Sort Success

--- src/PersonSort.java
+++ src/PersonSort.java
@@ -3,21 +3,43 @@ import java.util.Scanner;
 
 public class PersonSort extends Person {
 
+    public static void bubbleSort(ArrayList<Person> people){
+        boolean swapped;
+
+        for (int i = 0; i < people.size()-1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < people.size()-1; j++) {
+                if (people.get(j).getPersonAge() > people.get(j+1).getPersonAge()) {
+                    Person temp = people.get(j);
+                    people.set(j, people.get(j+1));
+                    people.set(j+1, temp);
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+
 
     public PersonSort(int personID, String firstName, String surName, int personAge) {
         super(personID, firstName, surName, personAge);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         ArrayList<Person> people = new ArrayList<>();
+
 
         System.out.println("How many people would you like to sort? ");
         int peopleNumber = scanner.nextInt();
         scanner.nextLine();
 
 
-        for (int i = 0; i<peopleNumber; i++){
+        for (int i = 0; i < peopleNumber; i++) {
             System.out.println("Enter the details of person: " + (i + 1));
 
             System.out.println("ID Number: ");
@@ -36,10 +58,16 @@ public class PersonSort extends Person {
             people.add(new Person(personID, firstName, surName, personAge));
         }
 
-        for (Person person: people){
+        for (Person person : people) {
             System.out.println(person);
+
+        }
+
+        bubbleSort(people);
+        for (Person person : people) {
+            System.out.println(person);
+
         }
 
     }
-
 }
